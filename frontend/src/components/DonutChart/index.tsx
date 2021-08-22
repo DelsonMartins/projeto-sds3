5dbacb4ebed1187daf7f284980c9eae88f0fc650
DelsonMartins <delsonmartins@terra.com.br>
2021-08-22 14:39:48 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sales";
 import { BASE_URL } from "utils/requests";
@@ -10,34 +11,41 @@ type ChartData = {
 
 const DonutChart = () => {
 
-  let chartData: ChartData = { labels: [], series:[] };
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-  axios.get(`${BASE_URL}/sales/amount-by-seller`)
-          .then( response => {
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+      .then(response => {
 
-              const data = response.data as SaleSum[];
-              const myLabels = data.map(x => x.sellerName);
-              const mySeries = data.map(x => x.sum);
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
 
-              chartData = { labels: myLabels, series: mySeries };
+        setChartData({ labels: myLabels, series: mySeries });
 
-              console.log (chartData);
-          });
+        //console.log (chartData);
+      });
+  }, []);
+
+  // Forma errada abaixo:
+  //let chartData: ChartData = { labels: [], series:[] };
+
+
 
   //const mockData = {
   //  series: [477138, 499928, 444867, 220426, 473088],
- //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-//}
+  //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  //}
 
-const options = {
+  const options = {
     legend: {
-        show: true
+      show: true
     }
-}
+  }
 
   return (
     <Chart
-      options={{ ...options, labels: chartData.labels}}
+      options={{ ...options, labels: chartData.labels }}
       series={chartData.series}
       type="donut"
       height="240"
